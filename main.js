ed2cb591683de82d4148ac45c5929bee64108a2b
NCC Lab User <NCC Lab User@Nash-Stu.nhctc.edu>
2021-10-08 10:17:28 -0400

    added demo code

---
+++ main.js
@@ -0,0 +1,11 @@
+/*
+ * Resources: 
+ *  - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
+ *  - https://github.com/mdn/js-examples/tree/master/modules
+ */
+
+// import the front-end code to handle math placement for the demonstration
+import { doMathPlacement } from "./ui/mathPlacement.js";
+
+// call the front-end
+doMathPlacement();
---
+++ modules/accuPlacer.js
@@ -0,0 +1,38 @@
+// back-end
+
+// bring in the configuration (note the naming conventions)
+import { Config } from '../config/accuPlacer.js';
+
+// create the class
+class AccuPlacer {
+    
+    // constructor (for the demo date isn't used)
+    constructor(date,testTaken,score) {
+        this.date = date;
+        this.testTaken = testTaken;
+        this.score = score;
+    }
+    
+    // handle the placement
+    placement() {
+        
+        // get all the thresholds - properties of the configuration
+        // test.taken is a property of the Config object
+        const thresholds = Object.keys(Config[this.testTaken]).reverse();
+        
+        // find the first threshold the score is greater than
+        const placement = thresholds.find(threshold => threshold < this.score);
+        
+        // "undefined" means the .find failed
+        if (placement !== "undefined") {
+            // return the value of the property (the placement)
+            return Config[this.testTaken][placement];
+        }
+        
+        // arguably unnecessary, but good for completeness
+        return "undefined";
+    }
+}
+
+// export so it can be used
+export { AccuPlacer };
