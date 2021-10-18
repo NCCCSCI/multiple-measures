087e94b214fd6771c4f9a896f2b5317fb26697e0
Bria Shorten <bshorten696@students.ccsnh.edu>
2021-10-18 15:50:20 -0400

    more html elements, more js added to test

--- modules/multiplemeasures.js
+++ modules/multiplemeasures.js
@@ -9,11 +9,16 @@ import { HighSchoolConfig } from '../config/mmhighschools.js';
 class MultipleMeasures {
     
     // constructor (for the demo date isn't used)
-    constructor(schoolname,gpa,englishclass,mathclass,weighted) {
-		
+    constructor(highschool,gpa,englishclass,englishscore,mathclass,mathscore) {
+		this._highschool = highschool;
+		this._gpa = gpa;
+		this._englishclass = englishclass;
+		this._englishscore = englishscore;
+		this._mathclass = mathclass;
+		this._mathscore = mathscore;
     }
 	
-	function calculatePercentage() {
+	calculatePercentage() {
 		
 	}
     
