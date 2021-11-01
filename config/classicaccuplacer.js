0cddd841cc86daf104c8dcaaca95231d1a7a25aa
Jose C Rueda Villanueva <jruedavillanueva646@students.ccsnh.edu>
2021-11-01 08:14:56 +0000

    updating classicaccuplacer.js config

--- config/classicaccuplacer.js
+++ config/classicaccuplacer.js
@@ -1,84 +1,45 @@
-// back-end
 
-//These functions  are going to help to determine their placement based on Classic accuplacer.
+const classicEnglishPlacement = {
+    //Writeplacer is nested inside Sentence Skills (minimum score required)
+    0: {
+        0: "**Nashua Adult Learning Center ABE",
+    },
 
-//Math Classic Accuplacer
-function accuplacermath(arit, quant) {
-    if(arit<25){
-        return 'Nashua Adult Learning Center ABE';
-                }
-      else {
-        if (arit<60) {
-            return 'MATH090 College Transition Program';
-        }
-          else {
-            if (quant<63) {
-                return 'Co - requsite Workshop with courses: MATH103N, MATH106N, MATH110N';
-            } else {
-                if (quant < 80) {
-                    return 'MATH103 or Co-Requisite Workshop with courses:MATH106N, MATH110N';
-                } else {
-                    if (quant > 80) {
-                    return 'MATH106N, MATH110N';
-                } else {
-                    return 'Not defined';
-                }
-            }
-                }
-        
+    41: {
+        0: "***ENGL090 College Transition Program",
+        4: "Refer to English Program Coordinator"
+    },
+
+    55: {
+        0: "Refer to English Program Coordinator",
+        4: "ENGL101N w/Co-Req",
+        5: "Refer to English Program Coordinator",
+    },
+
+    70: {
+        5: "ENGL101N",
+    },
+
+    95: {
+        6: "ENGL101N  or ENGL110N"
     }
-    }
-    //English Classic Accuplacer
-function accuplacereng(writting, essay) {
-    if (writting <= 40) {
-        return 'Nashua Adult Learning Center ABE';
-    }
-    else {
-        if (writting <= 54 && essay <= 3) {
-            return 'ENGL090 College Transition Program';//***Requires permission of the Honors Program Coordinator/Admission
+}
 
-        } else {
-            if (writting <= 54 && essay >= 4) {
-                return 'Refer to English Program Coordinator';
-            } else {
-                if (writting <= 69 && essay <= 3) {
-                    return 'Refer to English Profram Coordinator';
-                } else {
-                    if (writting <= 69 && essay == 4) {
-                        return 'ENGL101N w/ Co-Req';
-                    }
-                        else {
-                        if (writting <= 69 && essay >= 5) {
-                            return 'Refer to English Program Coordinator';
-                        } else {
-                            if (writting >= 70 && essay >= 5) {
-                                return 'ENGL101N';
-                            } else {
-                                if (writting >= 95 && essay >= 6) {
-                                    return 'ENGL101N or ENGL110N';
-                                }
-                                    else {
-                                    if (writting >= 70 && essay >= 5) {
-                                        return 'ENGL101N';
-                                    } else { 
-                                        return 'Not defined';
-                                    }
-                                    }
-                                
 
-                                
-                            }
-                        }
-                        }
-                    
-                }
+const classicMathPlacement = {
+    //Elem. Algebra is nested inside Arithmetic (minimum score required)
+    0: {
+        0: "Nashua Adult Learning Center ABE",
+    },
 
-            }
-                
+    25: {
+        0: "MATH090 College Transition Program"
+    },
 
-        }
-    }
-        
+    60: {
+        0: "Co-requisite Workshop with courses: MATH103N, MATH106N, MATH110N",
+        63: "MATH103 or Co-requisite Workshop with courses: MATH106N, MATH110N",
+        80: "MATH106N, MATH110N",
     }
 }
 
