4dbb416fc59065adb4452fd4013cfbe0de228d96
Jose C Rueda Villanueva <jruedavillanueva646@students.ccsnh.edu>
2021-11-01 08:18:10 +0000

    Separating accuplacers

--- config/classicaccuplacer.js
+++ config/classicaccuplacer.js
@@ -24,22 +24,4 @@ const classicEnglishPlacement = {
         6: "ENGL101N  or ENGL110N"
     }
 }
-
-
-const classicMathPlacement = {
-    //Elem. Algebra is nested inside Arithmetic (minimum score required)
-    0: {
-        0: "Nashua Adult Learning Center ABE",
-    },
-
-    25: {
-        0: "MATH090 College Transition Program"
-    },
-
-    60: {
-        0: "Co-requisite Workshop with courses: MATH103N, MATH106N, MATH110N",
-        63: "MATH103 or Co-requisite Workshop with courses: MATH106N, MATH110N",
-        80: "MATH106N, MATH110N",
-    }
-}
-
+export {classicEnglishPlacement};
