abc94eaf53f9dcea3301ca4f733a3f506a2bd0c1
Jose C Rueda Villanueva <jruedavillanueva646@students.ccsnh.edu>
2021-11-03 20:44:16 +0000

    Updated after talking with Chris

--- config/NextGenNonNativeEngAccuplacer.js
+++ config/NextGenNonNativeEngAccuplacer.js
@@ -1,22 +1,24 @@
 //Writeplacer is nested inside Writing Conventions (minimum score required)
-//Non native speakers only
+//Self-Identified Non native speakers only
 const EnglishPlacementNonNative = {
     0: {
         0: "Refer to Adult Leaming Center CTP Coordinator at NCC",
-        2: "ENGL090 College Transition Program (CTP), ESOL 086N *: Academic Writing for English Language Learners II",
+        2: "ENGL090 College Transition Program (CTP)(through Summer 2019), ESOL 086N *: Academic Writing for English Language Learners II (beginning Fall 2019)",
         4: "ESOL 088N*: Academic Writing for English Language Learners III"
     },
 
     237: {
-        0: "ENGL090 College Transition Program (CTP), ESOL 086N*: Academic Writing for English Language Learners II",
-        3: "ENGL101N w/ESOL Co-Req Please note: When Writeplacer is 3: ESOL088N * is strongly suggested OR Writeplacer is 4: ESOL096N * is strongly suggested.",
-        5: "ENGL1O1N w/ESOL Co-Req"
+        0: "ENGL090 College Transition Program (CTP)(through Summer 2019), ESOL 086N*: Academic Writing for English Language Learners II (beginning Fall 2019)",
+        3: "ENGL101N w/ESOL Co-Requisite WorkShop (Please note: When Writeplacer is 3: ESOL088N * is strongly suggested OR Writeplacer is 4: ESOL096N * is strongly suggested.)",
+        5: "ENGL1O1N w/ESOL Co-Requisite Workshop"
     },
 
     250: {
-        5: "ENGL101N"
+        0: "Refer to English Program Coordinator",
+        4: "ENGL101N"
     },
     263: {
+        0: "Refer to English Program Coordinator",
         5: "ENGL 101N",
         6: "ENGL101N or ENGL110N**"
     }
