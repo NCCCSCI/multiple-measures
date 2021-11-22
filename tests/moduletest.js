166c85971a161af4d0da4f0ab9be51f0aee23c6c
Bria Shorten <bshorten696@students.ccsnh.edu>
2021-11-22 16:26:10 -0500

    adding very basic testing for modules

---
+++ tests/moduletest.js
@@ -0,0 +1,22 @@
+// front-end
+
+// import the back-end code which performs the placement
+import { MultipleMeasuresEnglish } from '../modules/accuPlacerModule.js';
+
+
+let gobttn = document.getElementById("gobttn");
+
+function generatePlacement(e) {
+	let highschool = document.getElementById("highschool").value;
+	let score = document.getElementById("score").value;
+	let weighted = document.getElementById("weighted").checked;
+	let rec = document.getElementById("recommendation");
+	
+	const MultipleMeasuresEnglish = new MultipleMeasuresEnglish(highschool, score, weighted, "AP");
+	
+	const placement = MultipleMeasuresEnglish.englishplacement();
+	
+	rec.textContent = placement;
+}
+
+gobttn.addEventListener("click", generatePlacement);
