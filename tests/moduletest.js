4a7e44496a899b5f5a204ba86ac51063d32d3179
Bria Shorten <bshorten696@students.ccsnh.edu>
2021-11-22 16:39:25 -0500

    reducing amount of variables being tested

--- tests/moduletest.js
+++ tests/moduletest.js
@@ -9,10 +9,9 @@ let gobttn = document.getElementById("gobttn");
 function generatePlacement(e) {
 	let highschool = document.getElementById("highschool").value;
 	let score = document.getElementById("score").value;
-	let weighted = document.getElementById("weighted").checked;
 	let rec = document.getElementById("recommendation");
 	
-	const MultipleMeasuresEnglish = new MultipleMeasuresEnglish(highschool, score, weighted, "AP");
+	const MultipleMeasuresEnglish = new MultipleMeasuresEnglish(highschool, score, true, "AP");
 	
 	const placement = MultipleMeasuresEnglish.englishplacement();
 	
