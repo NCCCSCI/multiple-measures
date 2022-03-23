6c0dc95d79e38f36c3f80f1b9ed732d0c8a20e60
Jose Rueda <jruedavillanueva646@students.ccsnh.edu>
2022-03-23 15:48:25 +0000

    Changed string values to match mmmath.js variables.

--- config/HighSchoolCourseEquivalencies.js
+++ config/HighSchoolCourseEquivalencies.js
@@ -3,93 +3,93 @@
 
 const HSCourseEqConfig = {
     Merrimack: {
-        'Algebra I': "Algebra I Equivalent",
-        'Technical Math': "Algebra I Equivalent",
-        'Mathematics for College Placement': "Algebra I Equivalent",
-        'Algebra 2': "Algebra II Equivalent",
-        'Trigonometry': "Trigonometry Equivalent",
-        'Pre-AP Calculus': "Precalculus Equivalent"
+        'Algebra I': "AlgebraI",
+        'Technical Math': "AlgebraI",
+        'Mathematics for College Placement': "AlgebraI",
+        'Algebra 2': "AlgebraII_Statistics",
+        'Trigonometry': "Precalculus_Trigonometry",
+        'Pre-AP Calculus': "Precalculus_Trigonometry"
     },
     Alvirne: {
-        'Algebra 1 Part 1 & 2': "Algebra I Equivalent",
-        'Algebra 2': "Algebra II Equivalent",
-        'Trigonometry': "Trigonometry Equivalent",
-        'Precalculus': "Precalculus Equivalent"
+        'Algebra 1 Part 1 & 2': "AlgebraI",
+        'Algebra 2': "AlgebraII_Statistics",
+        'Trigonometry': "Precalculus_Trigonometry",
+        'Precalculus': "Precalculus_Trigonometry"
     },
     Mascenic: {
-        'Algebra 1': "Algebra I Equivalent",
-        'Senior Math': "Algebra I Equivalent",
-        'Algebra 2': "Algebra II Equivalent",
-        'Precalculus': "Precalculus Equivalent"
+        'Algebra 1': "AlgebraI",
+        'Senior Math': "AlgebraI",
+        'Algebra 2': "AlgebraII_Statistics",
+        'Precalculus': "Precalculus_Trigonometry"
     },
     Milford: {
-        'Algebra 1': "Algebra I Equivalent",
-        'Quantitative Reasoning': "Algebra I Equivalent",
-        'Algebra 2': "Algebra II Equivalent",
-        'Algebra III with Trigonometry': "Algebra III Equivalent",
-        'Precalculus': "Precalculus Equivalent"
+        'Algebra 1': "AlgebraI",
+        'Quantitative Reasoning': "AlgebraI",
+        'Algebra 2': "AlgebraII_Statistics",
+        'Algebra III with Trigonometry': "AlgebraIII",
+        'Precalculus': "Precalculus_Trigonometry"
     },
     Nashua_South: {
-        'Algebra 1 Part 1 & 2': "Algebra I Equivalent",
-        'Algebra 1': "Algebra I Equivalent",
-        'College Ready Math': "Algebra I Equivalent",
-        'Algebra 2': "Algebra II Equivalent",
-        'Advanced Math Topics': "Algebra III Equivalent",
-        'Precalculus': "Precalculus Equivalent"
+        'Algebra 1 Part 1 & 2': "AlgebraI",
+        'Algebra 1': "AlgebraI",
+        'College Ready Math': "AlgebraI",
+        'Algebra 2': "AlgebraII_Statistics",
+        'Advanced Math Topics': "AlgebraIII",
+        'Precalculus': "Precalculus_Trigonometry"
     },
     Nashua_North: {
-        'Algebra 1 Part 1 & 2': "Algebra I Equivalent",
-        'Algebra 1': "Algebra I Equivalent",
-        'College Ready Math': "Algebra I Equivalent",
-        'Algebra 2': "Algebra II Equivalent",
-        'Advanced Math Topics': "Algebra III Equivalent",
-        'Precalculus': "Precalculus Equivalent"
+        'Algebra 1 Part 1 & 2': "AlgebraI",
+        'Algebra 1': "AlgebraI",
+        'College Ready Math': "AlgebraI",
+        'Algebra 2': "AlgebraII_Statistics",
+        'Advanced Math Topics': "AlgebraIII",
+        'Precalculus': "Precalculus_Trigonometry"
     },
     Pelham: {
-        'Algebra 1': "Algebra I Equivalent",
-        'Applied Algebra': "Algebra I Equivalent",
-        'Algebra 2': "Algebra II Equivalent",
-        'Trigonometry': "Trigonometry Equivalent",
-        'Precalculus': "Precalculus Equivalent"
+        'Algebra 1': "AlgebraI",
+        'Applied Algebra': "AlgebraI",
+        'Algebra 2': "AlgebraII_Statistics",
+        'Trigonometry': "Precalculus_Trigonometry",
+        'Precalculus': "Precalculus_Trigonometry"
     },
     Campbell: {
-        'Algebra 1': "Algebra I Equivalent",
-        'Algebra 2': "Algebra II Equivalent",
-        'Precalculus': "Precalculus Equivalent"
+        'Algebra 1': "AlgebraI",
+        'Algebra 2': "AlgebraII_Statistics",
+        'Precalculus': "Precalculus_Trigonometry"
     },
     ConVal: {
-        'Algebra 1 Part 1 & 2': "Algebra I Equivalent",
-        'Algebra 2': "Algebra II Equivalent",
-        'Algebra 3': "Algebra III Equivalent",
-        'Trigonometry': "Trigonometry Equivalent",
-        'Algebra 3 & Trigonometry': "Precalculus Equivalent",
-        'Precalculus': "Precalculus Equivalent"
+        'Algebra 1 Part 1 & 2': "AlgebraI",
+        'Algebra 2': "AlgebraII_Statistics",
+        'Algebra 3': "AlgebraIII",
+        'Trigonometry': "Precalculus_Trigonometry",
+        'Algebra 3 & Trigonometry': "Precalculus_Trigonometry",
+        'Precalculus': "Precalculus_Trigonometry"
     },
     Wilton_Lyndenborough: {
-        'Basic Algebra A & B': "Algebra I Equivalent",
-        'Algebra 1': "Algebra I Equivalent",
-        'The Power of Math': "Algebra I Equivalent",
-        'Algebra 2': "Algebra II Equivalent",
-        'Precalculus': "Precalculus Equivalent"
+        'Basic Algebra A & B': "AlgebraI",
+        'Algebra 1': "AlgebraI",
+        'The Power of Math': "AlgebraI",
+        'Algebra 2': "AlgebraII_Statistics",
+        'Precalculus': "Precalculus_Trigonometry"
     },
     Hollis_Brookline: {
-        'Algebra 1 A & B': "Algebra I Equivalent",
-        'Algebra 1': "Algebra I Equivalent",
-        'Algebra 2': "Algebra II Equivalent",
-        'RS Algebra 2 with QR': "Algebra II Equivalent",
-        'RS Algebra 3/Trigonometry': "Algebra III Equivalent",
-        'Precalculus': "Precalculus Equivalent"
+        'Algebra 1 A & B': "AlgebraI",
+        'Algebra 1': "AlgebraI",
+        'Algebra 2': "AlgebraII_Statistics",
+        'RS Algebra 2 with QR': "AlgebraII_Statistics",
+        'RS Algebra 3/Trigonometry': "AlgebraIII",
+        'Precalculus': "Precalculus_Trigonometry"
     },
     Souhegan: {
-        'Core 2 Math': "Algebra I Equivalent",
-        'Core 3 Math': "Algebra II Equivalent",
-        'Precalculus': "Precalculus Equivalent",
-        'Intro to Calculus 1': "Precalculus Equivalent"
+        'Core 2 Math': "AlgebraI",
+        'Core 3 Math': "AlgebraII_Statistics",
+        'Precalculus': "Precalculus_Trigonometry",
+        'Intro to Calculus 1': "Precalculus_Trigonometry"
     },
     Conant: {
-        'Algebra 1': "Algebra I Equivalent",
-        'Algebra 2': "Algebra II Equivalent",
-        'Precalculus': "Precalculus Equivalent"
+        'Algebra 1': "AlgebraI",
+        'Algebra 2': "AlgebraII_Statistics",
+        'Precalculus': "Precalculus_Trigonometry"
     }
 }
 export { HSCourseEqConfig };
