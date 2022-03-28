728cd4968be57f7d725f320e82e541cb6431a47b
Nichole Francoeur <74684702+vierelements@users.noreply.github.com>
2022-03-28 11:15:50 -0400

    satScores.js no worksgit add .

--- controls/satScore.js
+++ controls/satScore.js
@@ -5,34 +5,30 @@ function sat(){
 
     // get sat math score input 
     const satMath =
-    document.getElementById("input-sat-3").value.trim();
+    document.getElementById("input-sat-2").value.trim();
 
     // get sat reading and writing input 
-    //const satReading =
-    //document.getElementById("input-sat-4").value.trim();
+    const satReading =
+    document.getElementById("input-sat-3").value.trim();
 
     // getting the sat thresholds
     const thresholdMath = Object.keys(SATConfig.Mathematics).reverse();
-    //const thresholdReading = Object.keys(SATConfig.Critical_Reading_and_Writing).reverse();
+    const thresholdReading = Object.keys(SATConfig.Critical_Reading_and_Writing).reverse();
 
-   
-    
     // finding the first threshold score
-    const mathPlacement = thresholdMath.find(thresholdMath => thresholdMath < satMath); 
-    //const readingPlacement = thresholdReading.find( thresholdReading => thresholdReading < satReading); 
-  
+    const mathPlacement = thresholdMath.find(thresholdMath =>  satMath > thresholdMath); 
+    const readingPlacement = thresholdReading.find( thresholdReading => satReading > thresholdReading); 
+
     if (mathPlacement !== "undefined") {
         // return math placement
-        return SATConfig.Mathematics[mathPlacement];
+        console.log(SATConfig.Mathematics[mathPlacement]);
     }
 
-
-    /*
    if (readingPlacement !== "undefined") {
          // return reading placement
-        return SATConfig.Critical_Reading_and_Writing[readingplacement];
+        return (SATConfig.Critical_Reading_and_Writing[readingPlacement]);
     }
-*/
+
     //null if nothings found
     return null; 
 }
