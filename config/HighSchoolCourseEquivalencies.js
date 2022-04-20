2984cf1a2d328ff238cde2a0549810a3dc2da411
Nichole Francoeur <74684702+vierelements@users.noreply.github.com>
2022-04-20 11:31:36 -0400

    took out spaces in hs course equivalencies

--- config/HighSchoolCourseEquivalencies.js
+++ config/HighSchoolCourseEquivalencies.js
@@ -3,92 +3,92 @@
 //Trigonometry and Precalculus equivalent value (NCC) are together inside Precalculus_Trigonometry variable.
 const HSCourseEqConfig = {
     Merrimack: {
-        'Algebra I': "Algebra I",
-        'Technical Math': "Algebra I",
-        'Mathematics for College Placement': "Algebra I",
-        'Algebra 2': "Algebra II_Statistics",
+        'Algebra I': "AlgebraI",
+        'Technical Math': "AlgebraI",
+        'Mathematics for College Placement': "AlgebraI",
+        'Algebra 2': "AlgebraII_Statistics",
         'Trigonometry': "Precalculus_Trigonometry",
         'Pre-AP Calculus': "Precalculus_Trigonometry"
     },
     Alvirne: {
-        'Algebra 1 Part 1 & 2': "Algebra I",
+        'Algebra 1 Part 1 & 2': "AlgebraI",
         'Algebra 2': "AlgebraII_Statistics",
         'Trigonometry': "Precalculus_Trigonometry",
         'Precalculus': "Precalculus_Trigonometry"
     },
     Mascenic: {
-        'Algebra 1': "Algebra I",
-        'Senior Math': "Algebra I",
+        'Algebra 1': "AlgebraI",
+        'Senior Math': "AlgebraI",
         'Algebra 2': "AlgebraII_Statistics",
         'Precalculus': "Precalculus_Trigonometry"
     },
     Milford: {
-        'Algebra 1': "Algebra I",
-        'Quantitative Reasoning': "Algebra I",
+        'Algebra 1': "AlgebraI",
+        'Quantitative Reasoning': "AlgebraI",
         'Algebra 2': "AlgebraII_Statistics",
-        'Algebra III with Trigonometry': "Algebra III",
+        'Algebra III with Trigonometry': "AlgebraIII",
         'Precalculus': "Precalculus_Trigonometry"
     },
     Nashua_South: {
-        'Algebra 1 Part 1 & 2': "Algebra I",
-        'Algebra 1': "Algebra I",
-        'College Ready Math': "Algebra I",
-        'Algebra 2': "Algebra II_Statistics",
-        'Advanced Math Topics': "Algebra III",
+        'Algebra 1 Part 1 & 2': "AlgebraI",
+        'Algebra 1': "AlgebraI",
+        'College Ready Math': "AlgebraI",
+        'Algebra 2': "AlgebraII_Statistics",
+        'Advanced Math Topics': "AlgebraIII",
         'Precalculus': "Precalculus_Trigonometry"
     },
     Nashua_North: {
-        'Algebra 1 Part 1 & 2': "Algebra I",
-        'Algebra 1': "Algebra I",
-        'College Ready Math': "Algebra I",
-        'Algebra 2': "Algebra II_Statistics",
-        'Advanced Math Topics': "Algebra III",
+        'Algebra 1 Part 1 & 2': "AlgebraI",
+        'Algebra 1': "AlgebraI",
+        'College Ready Math': "AlgebraI",
+        'Algebra 2': "AlgebraII_Statistics",
+        'Advanced Math Topics': "AlgebraIII",
         'Precalculus': "Precalculus_Trigonometry"
     },
     Pelham: {
-        'Algebra 1': "Algebra I",
-        'Applied Algebra': "Algebra I",
+        'Algebra 1': "AlgebraI",
+        'Applied Algebra': "AlgebraI",
         'Algebra 2': "AlgebraII_Statistics",
         'Trigonometry': "Precalculus_Trigonometry",
         'Precalculus': "Precalculus_Trigonometry"
     },
     Campbell: {
-        'Algebra 1': "Algebra I",
+        'Algebra 1': "AlgebraI",
         'Algebra 2': "AlgebraII_Statistics",
         'Precalculus': "Precalculus_Trigonometry"
     },
     ConVal: {
-        'Algebra 1 Part 1 & 2': "Algebra I",
-        'Algebra 2': "Algebra II_Statistics",
-        'Algebra 3': "Algebra III",
+        'Algebra 1 Part 1 & 2': "AlgebraI",
+        'Algebra 2': "AlgebraII_Statistics",
+        'Algebra 3': "AlgebraIII",
         'Trigonometry': "Precalculus_Trigonometry",
         'Algebra 3 & Trigonometry': "Precalculus_Trigonometry",
         'Precalculus': "Precalculus_Trigonometry"
     },
     Wilton_Lyndenborough: {
-        'Basic Algebra A & B': "Algebra I",
-        'Algebra 1': "Algebra I",
-        'The Power of Math': "Algebra I",
+        'Basic Algebra A & B': "AlgebraI",
+        'Algebra 1': "AlgebraI",
+        'The Power of Math': "AlgebraI",
         'Algebra 2': "AlgebraII_Statistics",
         'Precalculus': "Precalculus_Trigonometry"
     },
     Hollis_Brookline: {
-        'Algebra 1 A & B': "Algebra I",
-        'Algebra 1': "Algebra I",
-        'Algebra 2': "Algebra II_Statistics",
-        'RS Algebra 2 with QR': "Algebra II_Statistics",
-        'RS Algebra 3/Trigonometry': "Algebra III",
+        'Algebra 1 A & B': "AlgebraI",
+        'Algebra 1': "AlgebraI",
+        'Algebra 2': "AlgebraII_Statistics",
+        'RS Algebra 2 with QR': "AlgebraII_Statistics",
+        'RS Algebra 3/Trigonometry': "AlgebraIII",
         'Precalculus': "Precalculus_Trigonometry"
     },
     Souhegan: {
-        'Core 2 Math': "Algebra I",
-        'Core 3 Math': "Algebra II_Statistics",
+        'Core 2 Math': "AlgebraI",
+        'Core 3 Math': "AlgebraII_Statistics",
         'Precalculus': "Precalculus_Trigonometry",
         'Intro to Calculus 1': "Precalculus_Trigonometry"
     },
     Conant: {
-        'Algebra 1': "Algebra I",
-        'Algebra 2': "Algebra II_Statistics",
+        'Algebra 1': "AlgebraI",
+        'Algebra 2': "AlgebraII_Statistics",
         'Precalculus': "Precalculus_Trigonometry"
     }
 }
